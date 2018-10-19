37467177ba9f5bc32fc8edbefa476b790dc2f945
Kristen Robertson <kristen77@gmail.com>
2018-10-19 20:07:45 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -42,10 +42,9 @@ end
   
 def add_s(array)
   array.each_with_index.collect do |element, index| 
-  array.each_with_index.collect do |element, index|
     if index == 1
       element
     else
       element << "s"
-    end
   end
+end
